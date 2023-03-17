66c8b165b4512383736c2b4f8c3432fa993c2cbd
williamgriffiths <63455497+williamgriffiths@users.noreply.github.com>
2023-03-17 18:20:42 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -9,6 +9,10 @@ downloadButtons.forEach((button, index) => {
             sourceCodePath = 'Code/2. TVM Calculator/TVM.py';
         } else if (index === 2) {
             sourceCodePath = 'Code/3. Stock Portfolio Tracker/Portfolio Tracker.py';
+        } else if (index === 3) {
+            sourceCodePath = 'Code/4. NPV and IRR Calculator/NPV_IRR.py';
+        } else if (index === 4) {
+            sourceCodePath = 'Code/5. Sudoku/Sudoku Solver.py';
         } else {
             sourceCodePath = `path/to/project${index + 1}-source-code.zip`;
         }
