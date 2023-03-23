018e35fe73088038a62c69f3191a066788b441c6
williamgriffiths <63455497+williamgriffiths@users.noreply.github.com>
2023-03-23 21:57:21 +0000

    Update spots.py

--- code/Misc/6. Damien Hirst Spots/spots.py
+++ code/Misc/6. Damien Hirst Spots/spots.py
@@ -1,13 +1,27 @@
 import random
 from PIL import Image, ImageDraw
+import math
+
+def get_color_exclude(exclude_colors, available_colors):
+    remaining_colors = []
+    for color in available_colors:
+        too_similar = False
+        for exclude_color in exclude_colors:
+            distance = math.sqrt(sum((a - b) ** 2 for a, b in zip(color, exclude_color)))
+            if distance < 70:
+                too_similar = True
+                break
+        if not too_similar:
+            remaining_colors.append(color)
+
+    if not remaining_colors:
+        remaining_colors = [color for color in available_colors if color not in exclude_colors]
+
+    color = random.choice(remaining_colors)
+    return color, remaining_colors
 
 def generate_spots_artwork(rows, columns, output_filename):
-    spot_colors = [
-        (196, 2, 51), (239, 82, 159), (255, 200, 87), (0, 173, 239), (28, 172, 120),
-        (0, 104, 55), (0, 31, 63), (175, 111, 9), (48, 63, 159), (206, 17, 38),
-        (255,145,194), (255,118,37), (1,190,212), (47,225,229), (255,112,108),
-        (150,108,187), (210,240,249)
-    ]
+    spot_colors = [        (196, 2, 51), (239, 82, 159), (255, 200, 87), (0, 173, 239), (28, 172, 120),        (0, 104, 55), (0, 31, 63), (175, 111, 9), (48, 63, 159), (206, 17, 38),        (255,145,194), (255,118,37), (1,190,212), (47,225,229), (255,112,108),        (150,108,187), (210,240,249), (108,75,167), (138,188,247),   (254,135,0), (254,112,103)]
 
     spot_diameter = 1600 // (columns * 2 + 1)
     spot_radius = spot_diameter // 2
@@ -17,12 +31,22 @@ def generate_spots_artwork(rows, columns, output_filename):
     draw = ImageDraw.Draw(image)
 
     def get_color_exclude(exclude_colors, available_colors):
-        remaining_colors = [color for color in available_colors if color not in exclude_colors]
+        remaining_colors = []
+        for color in available_colors:
+            too_similar = False
+            for exclude_color in exclude_colors:
+                distance = math.sqrt(sum((a - b) ** 2 for a, b in zip(color, exclude_color)))
+                if distance < 60:
+                    too_similar = True
+                    break
+            if not too_similar:
+                remaining_colors.append(color)
+
         if not remaining_colors:
-            remaining_colors = [color for color in spot_colors if color not in exclude_colors]
+            remaining_colors = [color for color in available_colors if color not in exclude_colors]
+
         color = random.choice(remaining_colors)
         return color, remaining_colors
-
 
     available_colors_columns = [spot_colors.copy() for _ in range(columns)]
 
@@ -30,7 +54,6 @@ def generate_spots_artwork(rows, columns, output_filename):
         for col in range(columns):
             x = col * 2 * spot_diameter + spot_radius + spot_diameter
             y = row * 2 * spot_diameter + spot_radius + spot_diameter
-
             exclude_colors = []
             if col > 0:
                 left_color = image.getpixel((x - 2 * spot_diameter, y))
@@ -54,12 +77,19 @@ def generate_spots_artwork(rows, columns, output_filename):
 
     image.save(output_filename)
 
+
 if __name__ == '__main__':
     rows = 8
     columns = 10
-    output_folder = r'C:\Users\wgrif\Desktop\Website\Code\Misc\6. Damien Hirst Spots'
-    iterations = 10
-    for i in range(iterations):
-        output_path = f'{output_folder}\\spots_artwork_{i+1}.png'
-        generate_spots_artwork(rows, columns, output_path)
-        print(f"Created Spots {i+1}/{iterations}.")
+    output_folder = r'C:\Users\wgrif\Desktop\Website\Code\Misc\6. Damien Hirst Spots\images3'
+    iterations = 50
+    successful_drawings = 0
+
+    while successful_drawings < iterations:
+        output_path = f'{output_folder}\\spots_artwork_{successful_drawings+1}.png'
+        try:
+            generate_spots_artwork(rows, columns, output_path)
+            successful_drawings += 1
+            print(f"Created Spots {successful_drawings}/{iterations}.")
+        except IndexError:
+            continue
